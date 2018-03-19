f7ebd930a5a5c257ccdb4b23e9af3bddc031b3fb
PekkaSimola <pekka1simola@gmail.com>
2018-03-19 14:48:32 +0100

    Pekka first classes

---
+++ src/se/lexicon/model/FoodAllergy.java
@@ -0,0 +1,5 @@
+package se.lexicon.model;
+
+public enum FoodAllergy {
+	GLUTEN, LACTOS, NUTS, SEE_NOTES
+}
---
+++ src/se/lexicon/model/Passenger.java
@@ -0,0 +1,13 @@
+package se.lexicon.model;
+
+public class Passenger {
+
+	int seatNumber; //Business: 1 to 5; Economy: 6 to 10
+	GenderType gender;
+	int age;
+	String fellowPassanger;
+	String passportNumber;
+	String note;
+	String foodAllergies;
+	
+}
